40ff4491faeb2c359f39ffd1f2bfe7f625a516d1
Sascha Wise <me@saschawise.com>
2019-11-26 21:57:39 +1300

    Update postgres to Rust 2018 and std::futures

--- l337-postgres/src/lib.rs
+++ l337-postgres/src/lib.rs
@@ -1,6 +1,5 @@
-//! Postgres adapater for l3-37 pool
+///! Postgres adapater for l3-37 pool
 // #![deny(missing_docs, missing_debug_implementations)]
-
 extern crate futures;
 pub extern crate l337;
 extern crate tokio;
@@ -9,9 +8,12 @@ pub extern crate tokio_postgres;
 #[macro_use]
 extern crate log;
 
-use futures::sync::oneshot;
-use futures::{Async, Future, Stream};
+//use futures::sync::oneshot;
+use async_trait::async_trait;
+use futures::{stream::Stream, FutureExt};
+use std::future::Future;
 use tokio::executor::spawn;
+use tokio::sync::oneshot;
 use tokio_postgres::error::Error;
 use tokio_postgres::{
     tls::{MakeTlsConnect, TlsConnect},
@@ -23,7 +25,6 @@ use std::fmt;
 pub struct AsyncConnection {
     pub client: Client,
     broken: bool,
-    receiver: oneshot::Receiver<bool>,
 }
 
 /// A `ManageConnection` for `tokio_postgres::Connection`s.
@@ -47,7 +48,7 @@ where
         }
     }
 }
-
+#[async_trait]
 impl<T> l337::ManageConnection for PostgresConnectionManager<T>
 where
     T: 'static + MakeTlsConnect<Socket> + Clone + Send + Sync,
@@ -57,71 +58,29 @@ where
 {
     type Connection = AsyncConnection;
     type Error = Error;
-
-    fn connect(
-        &self,
-    ) -> Box<Future<Item = Self::Connection, Error = l337::Error<Self::Error>> + 'static + Send>
-    {
-        Box::new(
-            self.config
-                .connect(self.make_tls_connect.clone())
-                .map(|(client, connection)| {
-                    let (sender, receiver) = oneshot::channel();
-                    spawn(connection.map_err(|_| {
-                        sender
-                            .send(true)
-                            .unwrap_or_else(|e| panic!("failed to send shutdown notice: {}", e));
-                    }));
-                    AsyncConnection {
-                        broken: false,
-                        client,
-                        receiver,
-                    }
-                })
-                .map_err(|e| l337::Error::External(e)),
-        )
+    async fn connect(&self) -> Result<Self::Connection, l337::Error<Self::Error>> {
+        println!("Spawning postgres connection");
+        let result = self.config.connect(self.make_tls_connect.clone()).await;
+        println!("Connected to postgres db");
+        let (client, connection) = result.map_err(|err| l337::Error::External(err))?;
+        spawn(connection.map(|_| {}));
+        println!("Returning connection");
+        Ok(AsyncConnection {
+            broken: false,
+            client,
+        })
     }
-
-    fn is_valid(
-        &self,
-        mut conn: Self::Connection,
-    ) -> Box<Future<Item = (), Error = l337::Error<Self::Error>>> {
+    async fn is_valid(&self, conn: Self::Connection) -> Result<(), l337::Error<Self::Error>> {
         // If we can execute this without erroring, we're definitely still connected to the database
-        Box::new(
-            conn.client
-                .simple_query("")
-                .into_future()
-                .map(|_| ())
-                .map_err(|(e, _)| l337::Error::External(e)),
-        )
+        conn.client
+            .simple_query("")
+            .await
+            .map_err(|e| l337::Error::External(e))?;
+        Ok(())
     }
 
     fn has_broken(&self, conn: &mut Self::Connection) -> bool {
-        if conn.broken {
-            return true;
-        }
-
-        // Use try_recv() as `has_broken` can be called via Drop
-        // and not have a future Context to poll on.
-        // https://docs.rs/futures/0.1.28/futures/sync/oneshot/struct.Receiver.html#method.try_recv
-        match conn.receiver.try_recv() {
-            // If we get any message, the connection task stopped, which means this connection is
-            // now dead
-            Ok(Some(_)) => {
-                conn.broken = true;
-                true
-            }
-            // If the future isn't ready, then we haven't sent a value which means the future is
-            // still successfully running
-            Ok(None) => false,
-            // This can happen if the future that the connection was
-            // spawned in panicked or was dropped.
-            Err(err) => {
-                warn!("cannot receive from connection future - err: {}", err);
-                conn.broken = true;
-                true
-            }
-        }
+        conn.client.is_closed()
     }
 
     fn timed_out(&self) -> l337::Error<Self::Error> {
@@ -144,7 +103,7 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
-    use futures::Stream;
+    use futures::{join, Stream};
     use l337::{Config, Pool};
     use std::thread::sleep;
     use std::time::Duration;
@@ -161,22 +120,17 @@ mod tests {
 
         let mut runtime = Runtime::new().expect("could not run");
         let config: Config = Default::default();
-        let future = Pool::new(mngr, config).and_then(|pool| {
-            pool.connection().and_then(|mut conn| {
-                conn.client
-                    .prepare("SELECT 1::INT4")
-                    .and_then(move |select| {
-                        conn.client.query(&select, &[]).for_each(|row| {
-                            assert_eq!(1, row.get::<_, i32>(0));
-                            Ok(())
-                        })
-                    })
-                    .map(|connection| ((), connection))
-                    .map_err(|e| l337::Error::External(e))
-            })
+        runtime.block_on(async {
+            let pool: Pool<PostgresConnectionManager<tokio_postgres::NoTls>> =
+                Pool::new(mngr, config).await.unwrap();
+            let conn = pool.connection().await.unwrap();
+            let select = conn.client.prepare("SELECT 1::INT4").await.unwrap();
+            let rows = conn.client.query(&select, &[]).await.unwrap();
+            for row in rows {
+                assert_eq!(1, row.get::<_, i32>(0));
+                ()
+            }
         });
-
-        runtime.block_on(future).expect("could not run");
     }
 
     #[test]
@@ -190,43 +144,35 @@ mod tests {
 
         let mut runtime = Runtime::new().expect("could not run");
         let config: Config = Default::default();
-        let future = Pool::new(mngr, config).and_then(|pool| {
-            let q1 = pool.connection().and_then(|mut conn| {
-                conn.client
-                    .prepare("SELECT 1::INT4")
-                    .and_then(move |select| {
-                        conn.client.query(&select, &[]).for_each(|row| {
-                            assert_eq!(1, row.get::<_, i32>(0));
-                            Ok(())
-                        })
-                    })
-                    .map(|connection| {
-                        sleep(Duration::from_secs(5));
-                        ((), connection)
-                    })
-                    .map_err(|e| l337::Error::External(e))
-            });
+        runtime.block_on(async {
+            let pool: Pool<PostgresConnectionManager<tokio_postgres::NoTls>> =
+                Pool::new(mngr, config).await.unwrap();
+            let q1 = async {
+                let conn = pool.connection().await.unwrap();
+                let select = conn.client.prepare("SELECT 1::INT4").await.unwrap();
+                let rows = conn.client.query(&select, &[]).await.unwrap();
+                for row in rows {
+                    assert_eq!(1, row.get::<_, i32>(0));
+                    ()
+                }
+                //sleep(Duration::from_secs(5));
+            };
+            let q2 = async {
+                let conn = pool.connection().await.unwrap();
+                println!("Q2 got connection");
+                let select = conn.client.prepare("SELECT 2::INT4").await.unwrap();
+                let rows = conn.client.query(&select, &[]).await.unwrap();
+                for row in rows {
+                    assert_eq!(2, row.get::<_, i32>(0));
+                    ()
+                }
 
-            let q2 = pool.connection().and_then(|mut conn| {
-                conn.client
-                    .prepare("SELECT 2::INT4")
-                    .and_then(move |select| {
-                        conn.client.query(&select, &[]).for_each(|row| {
-                            assert_eq!(2, row.get::<_, i32>(0));
-                            Ok(())
-                        })
-                    })
-                    .map(|connection| {
-                        sleep(Duration::from_secs(5));
-                        ((), connection)
-                    })
-                    .map_err(|e| l337::Error::External(e))
-            });
-
-            q1.join(q2)
+                //sleep(Duration::from_secs(5));
+            };
+            //join!(q1, q2) // This is hanging for some reason. Not sure why
+            q1.await;
+            q2.await;
         });
-
-        runtime.block_on(future).expect("could not run");
     }
 
     #[test]
@@ -240,58 +186,45 @@ mod tests {
 
         let mut runtime = Runtime::new().expect("could not run");
         let config: Config = Default::default();
-        let future = Pool::new(mngr, config).and_then(|pool| {
-            let q1 = pool.connection().and_then(|mut conn| {
-                conn.client
-                    .prepare("SELECT 1::INT4")
-                    .and_then(move |select| {
-                        conn.client.query(&select, &[]).for_each(|row| {
-                            assert_eq!(1, row.get::<_, i32>(0));
-                            Ok(())
-                        })
-                    })
-                    .map(|connection| {
-                        sleep(Duration::from_secs(5));
-                        ((), connection)
-                    })
-                    .map_err(|e| l337::Error::External(e))
-            });
-
-            let q2 = pool.connection().and_then(|mut conn| {
-                conn.client
-                    .prepare("SELECT 2::INT4")
-                    .and_then(move |select| {
-                        conn.client.query(&select, &[]).for_each(|row| {
-                            assert_eq!(2, row.get::<_, i32>(0));
-                            Ok(())
-                        })
-                    })
-                    .map(|connection| {
-                        sleep(Duration::from_secs(5));
-                        ((), connection)
-                    })
-                    .map_err(|e| l337::Error::External(e))
-            });
-
-            let q3 = pool.connection().and_then(|mut conn| {
-                conn.client
-                    .prepare("SELECT 3::INT4")
-                    .and_then(move |select| {
-                        conn.client.query(&select, &[]).for_each(|row| {
-                            assert_eq!(3, row.get::<_, i32>(0));
-                            Ok(())
-                        })
-                    })
-                    .map(|connection| {
-                        sleep(Duration::from_secs(5));
-                        ((), connection)
-                    })
-                    .map_err(|e| l337::Error::External(e))
-            });
-
-            q1.join3(q2, q3)
+        runtime.block_on(async {
+            let pool: Pool<PostgresConnectionManager<tokio_postgres::NoTls>> =
+                Pool::new(mngr, config).await.unwrap();
+            let q1 = async {
+                let conn = pool.connection().await.unwrap();
+                let select = conn.client.prepare("SELECT 1::INT4").await.unwrap();
+                let rows = conn.client.query(&select, &[]).await.unwrap();
+                for row in rows {
+                    assert_eq!(1, row.get::<_, i32>(0));
+                    ()
+                }
+                //sleep(Duration::from_secs(5));
+            };
+            let q2 = async {
+                let conn = pool.connection().await.unwrap();
+                println!("Q2 got connection");
+                let select = conn.client.prepare("SELECT 2::INT4").await.unwrap();
+                let rows = conn.client.query(&select, &[]).await.unwrap();
+                for row in rows {
+                    assert_eq!(2, row.get::<_, i32>(0));
+                    ()
+                }
+                //sleep(Duration::from_secs(5));
+            };
+            let q3 = async {
+                let conn = pool.connection().await.unwrap();
+                println!("Q2 got connection");
+                let select = conn.client.prepare("SELECT 3::INT4").await.unwrap();
+                let rows = conn.client.query(&select, &[]).await.unwrap();
+                for row in rows {
+                    assert_eq!(3, row.get::<_, i32>(0));
+                    ()
+                }
+                //sleep(Duration::from_secs(5));
+            };
+            //join!(q1, q2, q3) // This is hanging for some reason. Not sure why
+            q1.await;
+            q2.await;
+            q3.await;
         });
-
-        runtime.block_on(future).expect("could not run");
     }
 }

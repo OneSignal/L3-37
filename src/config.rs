2aba312fb501aa709aa86a26bf243a4718dc349c
Nate Mara <nate@onesignal.com>
2020-04-08 10:17:51 -0700

    Add the ability to configure connection timeouts
    Previously, we did not directly support connection timeouts, relying on
    the end-user to call timeout on the returned future. It will be easier
    for users to configure a default timeout that will be used for all
    connection attempts. The default behavior is unchanged, if you do not
    configure a timeout, there will be no timeout.

--- src/config.rs
+++ src/config.rs
@@ -1,15 +1,28 @@
+use std::time::Duration;
+
 /// Configuration for the connection pool
 #[derive(Debug)]
 pub struct Config {
     pub(crate) min_size: usize,
     pub(crate) max_size: usize,
     pub(crate) test_on_check_out: bool,
+    pub(crate) connect_timeout: Option<Duration>,
 }
 
 impl Config {
     /// Create a new configuration object with default values
     pub fn new() -> Self {
         Self::default()
+    }
+
+    /// Set timeout period for starting up a new connection. If it takes longer
+    /// than the specified time to wait for a connection to become available, it
+    /// will fail.
+    ///
+    /// By default, there is no timeout limit on getting a new connection.
+    pub fn connection_timeout(mut self, timeout: Duration) -> Self {
+        self.connect_timeout = Some(timeout);
+        self
     }
 
     /// If true, the health of a connection will be verified via a call to
@@ -23,12 +36,16 @@ impl Config {
 
     /// Minimum number of connections in the pool. The pool will be initialied with this number of
     /// connections
+    ///
+    /// Defaults to 1 connection.
     pub fn min_size(mut self, min_size: usize) -> Self {
         self.min_size = min_size;
         self
     }
 
     /// Max number of connections to keep in the pool
+    ///
+    /// Defaults to 10 connections.
     pub fn max_size(mut self, max_size: usize) -> Self {
         self.max_size = max_size;
         self
@@ -41,6 +58,7 @@ impl Default for Config {
             max_size: 10,
             min_size: 1,
             test_on_check_out: true,
+            connect_timeout: None,
         }
     }
 }
